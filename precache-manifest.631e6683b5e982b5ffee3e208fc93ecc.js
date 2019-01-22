84c72ed8f29de606dbeb28b1dccd007f86c703ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-22 11:54:48 +0000

    Deploy J-DK/pr-dashboard to github.com/J-DK/pr-dashboard.git:gh-pages

---
+++ precache-manifest.631e6683b5e982b5ffee3e208fc93ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfc2a6d49dd0400601a2",
+    "url": "/pr-dashboard/static/css/main.7bcc11a2.chunk.css"
+  },
+  {
+    "revision": "cfc2a6d49dd0400601a2",
+    "url": "/pr-dashboard/static/js/main.cfc2a6d4.chunk.js"
+  },
+  {
+    "revision": "ba2c3fb90d73a32f6320",
+    "url": "/pr-dashboard/static/css/1.e7118130.chunk.css"
+  },
+  {
+    "revision": "ba2c3fb90d73a32f6320",
+    "url": "/pr-dashboard/static/js/1.ba2c3fb9.chunk.js"
+  },
+  {
+    "revision": "c6b3eac3a8cbe72f48ce",
+    "url": "/pr-dashboard/static/js/runtime~main.c6b3eac3.js"
+  },
+  {
+    "revision": "483edd786f833893074be329af266441",
+    "url": "/pr-dashboard/index.html"
+  }
+];
