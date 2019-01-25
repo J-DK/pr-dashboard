27daa965b3bbd1417dc3c9d6bc4d703fc4c92ea6
J-DK <dileepkumarjami@gmail.com>
2019-01-25 19:58:34 +0530

    Placed repo modal in dashboard

--- src/view/Dashboard/Dashboard.tsx
+++ src/view/Dashboard/Dashboard.tsx
@@ -2,13 +2,30 @@ import * as React from 'react';
 import Header from '../../components/Header/Header';
 import {AppService} from '../../services/app.service';
 import AddRepoButton from '../../components/AddRepoButton/AddRepoButton';
+import {InputRepoModal} from '../../components/InputRepoModal/InputRepoModal';
+import {IDashboardState} from './index';
 
-export class Dashboard extends React.Component {
+export class Dashboard extends React.Component<{}, IDashboardState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  };
+
   render() {
     return (
         <div>
           <Header />
-          <AddRepoButton title='Add Repo'/>
+          <AddRepoButton title='Add Repo' onClick={this.toggle}/>
+          <InputRepoModal showModal={this.state.showModal} onClick={this.toggle} onHide={this.toggle}/>
         </div>
     );
   }
